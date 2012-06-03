5b95af92d713b4e7866c5639737c65a9a74f7ed0
Alexey Ivanov <ivanbl4.geeks@gmail.com>
2012-06-03 18:27:09 +0400

    add websocketpp lib to project.

--- setup_vs_project_variables.bat
+++ setup_vs_project_variables.bat
@@ -2,6 +2,7 @@
 @echo off
 set BOOST_DIR=c:\libraries\boost\boost_1_48_0
 ::set BOOST_DIR=c:\libraries\boost\boost_1_46_1
+set WEBSOCKETPP_DIR=c:\libraries\websocketpp\zaphoyd-websocketpp-232fd1e
 set FREEIMAGELIB_DIR=c:\libraries\FreeImage\3.15.0
 ::set AIMP_PLUGINS_DIR=c:\AIMP2\Plugins
 set AIMP_PLUGINS_DIR=c:\AIMP3\Plugins
