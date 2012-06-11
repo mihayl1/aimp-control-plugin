64521c4dafe68a9f6cc53199a62b2c6c5d208eff
Alexey <Alexey@QWERTYMACHINE1>
2012-06-12 03:20:44 +0400

    upgrade to windows x64.

--- setup_environment.bat
+++ setup_environment.bat
@@ -1,20 +1,22 @@
 :: setup paths of all tools used in plugin build process.
 
 rem call "c:\Program Files\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
-call "c:\Program Files\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
+rem call "c:\Program Files\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
+call "c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 
-:: java runtime is used by yui_compressor and google closure compiler in MINIMIZE_CLIENT_SRC task.
-set JAVA_HOME=%JAVA_HOME%
+:: java runtime is used by yui_compressor and google closure compiler in MINIMIZE_CLIENT_SRC task. Currently jre7 does not work in pair with python scripts, so use jre6. 
+set JAVA_HOME=c:\Program Files\Java\jdk1.6.0_32
 
 set SUBWCREV_TOOL_DIR=c:\Program Files\TortoiseSVN\bin
-set INNO_SETUP_HOME=c:\Program Files\Inno Setup 5
-set PATH=%SUBWCREV_TOOL_DIR%;%INNO_SETUP_HOME%;%PATH%
+set INNO_SETUP_HOME=c:\Program Files (x86)\Inno Setup 5
+set PATH=%JAVA_HOME%\bin;%SUBWCREV_TOOL_DIR%;%INNO_SETUP_HOME%;%PATH%
 
 :: following variables are used for source indexing and storing debug symbols to symbol server.
-set PERL_DIR=c:\perl
-set DEBUG_TOOL_DIR=c:\Program Files\Debugging Tools for Windows (x86)
+set PERL_DIR=c:\Perl64
+::set DEBUG_TOOL_DIR=c:\Program Files\Debugging Tools for Windows (x86)
+set DEBUG_TOOL_DIR=c:\Program Files (x86)\Windows Kits\8.0\Debuggers\x64
 set SRCSRV_HOME=%DEBUG_TOOL_DIR%\srcsrv
-set SVN_CLIENT_DIR=c:\Program Files\Subversion\bin
+set SVN_CLIENT_DIR=c:\Program Files (x86)\Subversion\bin
 set PATH=%DEBUG_TOOL_DIR%;%SRCSRV_HOME%;%PERL_DIR%\bin;%SVN_CLIENT_DIR%\bin;%PATH%
 set SYMBOL_STORE=C:\Symbols
 
