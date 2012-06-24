0644fc310f85a5b3cc72c95ce22fd214a4100e7f
Alexey <Alexey@QWERTYMACHINE1>
2012-06-24 18:46:56 +0400

    integrating websocket server.

--- src/websocket/websocket_server.cpp
+++ src/websocket/websocket_server.cpp
@@ -2,6 +2,9 @@
 #include "websocket/server.h"
 #include "plugin/logger.h"
 
+#include <websocketpp.hpp>
+#include "endpoint.h"
+
 namespace {
 using namespace ControlPlugin::PluginLogger;
 ModuleLoggerType& logger()
@@ -13,15 +16,40 @@ namespace Websocket
 
 using ControlPlugin::PluginLogger::LogManager;
 
-Server::Server( boost::asio::io_service& /*io_service*/,
-                const std::string& /*address*/,
-                const std::string& /*port*/,
-                RequestHandler& request_handler
-              )
-    :
-    request_handler_(request_handler)
+typedef aimp_endpoint<websocketpp::role::server,
+                                   websocketpp::socket::plain> server;
+
+class echo_server_handler : public server::handler {
+public:
+    void on_message(connection_ptr con, message_ptr msg) {
+        con->send(msg->get_payload(), msg->get_opcode());
+    }
+};
+
+struct Server::impl : boost::noncopyable
 {
-    
+	impl(Rpc::RequestHandler& request_handler
+		 )
+        :
+        request_handler_(request_handler),
+        server_( server::handler::ptr( new echo_server_handler() ) )
+	{
+
+	}
+
+    Rpc::RequestHandler& request_handler_;
+    server server_;
+};
+
+Server::Server( const std::string& address,
+                const std::string& port,
+                Rpc::RequestHandler& request_handler
+               )
+    :
+	impl_( new impl(request_handler) )
+{
+    address;
+    port;
 }
 
 Server::~Server()
